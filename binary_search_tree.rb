e6e0053a9500f93bce3bb3620eb2c278b5df7dc0
Ben Daniels <benbanj@hotmail.com>
2019-11-21 16:31:48 +0000

    Adding insert and build tree methods

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -14,3 +14,41 @@ include Comparable
   end
      
 end
+
+class Tree
+  attr_accessor :root
+  
+    def initialize
+      @root = nil
+    end
+  
+    def build_tree(data)
+      data.uniq.each do |num|
+        insert(num, root)
+      end
+      root
+    end
+  
+    def insert(num, node)
+      if root.nil?
+        self.root = Node.new(num)
+      elsif num > node.data 
+        if node.right.nil?
+          node.right = Node.new(num)
+        else
+          insert(num, node.right)
+        end
+      elsif num < node.data
+        if node.left.nil?
+          node.left = Node.new(num)
+        else
+          insert(num, node.left)
+        end
+      end
+      root
+    end
+  
+    def delete(value)
+    end
+  
+  end
